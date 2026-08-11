a55cae51f0c52717157154e1bf8f4446e966bda3
Nicolas <abadia.pinheiro@gmail.com>
2026-08-11 18:55:05 -0300

    Add player list and detail views with tests

--- players/tests.py
+++ players/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Player
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class PlayerIndexViewTests(TestCase):
+    def test_list_all_players(self):
+        """
+        All players are displayed on the index page in correct order.
+        """
+        player1 = Player.objects.create(name="Player01", rating=1300)
+        player2 = Player.objects.create(name="Player02", rating=1900)
+
+        response = self.client.get(reverse("players:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerySetEqual(response.context["player_list"], [player2, player1])
+
+    def test_list_no_players(self):
+        """
+        No players are displayed if no players exist in db.
+        """
+        response = self.client.get(reverse("players:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerySetEqual(response.context["player_list"], [])
--- players/views.py
+++ players/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Player
 
 # Create your views here.
+
+
+class PlayerIndexView(generic.ListView):
+    """
+        Return a list of all players registered in the system ordered by rating.
+    """
+    model = Player
+    ordering = "-rating"
+    template_name = "players/index.html"
+    context_object_name = "player_list"
+
+
+class PlayerDetailView(generic.DetailView):
+    model = Player
+    template_name = "players/detail.html"
+    context_object_name = "player"

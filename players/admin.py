e48ed9d57c6f84181b932adebb5c86ab0e7876db
Nicolas <abadia.pinheiro@gmail.com>
2026-08-11 18:46:08 -0300

    Add Player model, migration, and admin registration

--- players/admin.py
+++ players/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Player
+
 # Register your models here.
+
+class PlayerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["name"]}),
+        ("Rating", {"fields": ["rating"]}),
+        ("Created Time", {"fields": ["created_date"]}),
+    ]
+    readonly_fields = ["created_date"]
+    list_display = ["name", "rating", "created_date"]
+    list_filter = ["rating", "created_date"]
+    search_fields = ["name"]
+
+
+admin.site.register(Player, PlayerAdmin)
--- players/models.py
+++ players/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    rating = models.IntegerField(default=1200)
+    created_date = models.DateTimeField("created date", auto_now_add=True)
+
+    def __str__(self):
+        return self.name

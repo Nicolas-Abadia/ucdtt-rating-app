7fd37443bd0ed7c16fc0992e9e5da96bd646e521
Nicolas <abadia.pinheiro@gmail.com>
2026-08-17 10:45:44 -0300

    Add Match model to Admin

--- players/admin.py
+++ players/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Player
+from .models import Player, Match
 
 # Register your models here.
 
@@ -15,5 +15,16 @@ class PlayerAdmin(admin.ModelAdmin):
     list_filter = ["rating", "created_date"]
     search_fields = ["name"]
 
+class MatchAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["player1", "player2"]}),
+        ("Score", {"fields": ["score1", "score2"]}),
+        ("Date", {"fields": ["date"]})
+    ]
+    list_display = ["id", "player1", "player2", "score1","score2", "date"]
+    list_display_links = ["player1", "player2"]
+    list_filter = ["date"]
+    search_fields = ["player1__name", "player2__name"]
 
 admin.site.register(Player, PlayerAdmin)
+admin.site.register(Match, MatchAdmin)

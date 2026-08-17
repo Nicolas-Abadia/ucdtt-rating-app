c22e964924a99ada2a2bf68672b042a4d163df34
Nicolas <abadia.pinheiro@gmail.com>
2026-08-16 21:47:45 -0300

    Add MinValueValidator for player's rating field

--- players/models.py
+++ players/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -5,7 +6,7 @@ from django.db import models
 
 class Player(models.Model):
     name = models.CharField(max_length=200)
-    rating = models.IntegerField(default=1200)
+    rating = models.IntegerField(default=1200, validators=[MinValueValidator(100)])
     created_date = models.DateTimeField("created date", auto_now_add=True)
 
     def __str__(self):

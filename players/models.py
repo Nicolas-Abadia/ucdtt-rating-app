ce7d85b983fbe9ab4bcc2de51ed6c444f2281d41
Nicolas <abadia.pinheiro@gmail.com>
2026-08-18 12:57:19 -0300

    Add rating history model and initial rating for players (from USATT or others)

--- players/models.py
+++ players/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(max_length=200)
     rating = models.IntegerField(default=1200, validators=[MinValueValidator(100)])
+    initial_rating = models.IntegerField(default=1200, validators=[MinValueValidator(100)])
     created_date = models.DateTimeField("created date", auto_now_add=True)
 
     def __str__(self):
@@ -21,3 +22,15 @@ class Match(models.Model):
 
     def __str__(self):
         return f"{self.player1} vs {self.player2} ({self.score1}-{self.score2}) on {self.date}"
+
+class RatingHistory(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="rating_history")
+    match = models.ForeignKey(Match, on_delete=models.PROTECT, null=True, blank=True, related_name="rating_changes")
+    rating = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return f"{self.player.name}: {self.rating}"

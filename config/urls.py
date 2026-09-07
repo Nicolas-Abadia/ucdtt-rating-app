f4d09b306795bcd9e247a0c6133f63442d65199b
Nicolas <abadia.pinheiro@gmail.com>
2026-09-07 17:39:53 -0300

    Add JWT authentication and CORS configuration

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from players.api import router
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("api/", include(router.urls)),
@@ -29,4 +30,6 @@ urlpatterns = [
     ),
     path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
